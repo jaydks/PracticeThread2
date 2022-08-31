010e92f979b1f6da65007608bbfd90b9164b2e3a
jaydks <jjeee1313@gmail.com>
2022-09-01 04:23:21 +0900

    타이머 - 핸들러 사용1

--- app/src/main/java/com/example/practicethread2/MainActivity.kt
+++ app/src/main/java/com/example/practicethread2/MainActivity.kt
@@ -9,8 +9,10 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityMainBinding
 
+    var total = 0 // 전체 시간을 저장하는 변수
     var started = false// 타미어가 실행중임을 알려주기 위한 변수
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,22 +31,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    val SET_TIME = 51
-    val RESET = 52
 
     val handler = object : Handler(){
         override fun handleMessage(msg: Message) {
-            when(msg.what){
-                SET_TIME -> {
-                    val total = msg.arg1 //val total = msg.what : sendemptymessage를 통해 전달된 값을 what을 통해 꺼내 쓸 수 있음. (what이 그 용도로 제공되는 건 아닌데 암튼 그렇게 쓸 수 있음. what은 명령어를 구분하기 위해 사용하는 용도)
-                    binding.textTimer.text = formatTime(total)
+            val total = msg.what //val total = msg.what : sendemptymessage를 통해 전달된 값을 what을 통해 꺼내 쓸 수 있음. (what이 그 용도로 제공되는 건 아닌데 암튼 그렇게 쓸 수 있음. what은 명령어를 구분하기 위해 사용하는 용도)
+            binding.textTimer.text = formatTime(total)
 
-                }
-                RESET -> {
-                    binding.textTimer.text = "00:00"
-                }
-
-            }
 
         }
     }
@@ -53,16 +45,11 @@ class MainActivity : AppCompatActivity() {
         started = true
         // sub thread (서브 스레드에선 ui에 직접 접근할 수 없음)
         thread(start = true) { // start : 쓰레드에 시작하라고 명령을 하는 것. thread가 가지고 있는 start라는 파라미터에 true세팅한 것
-            var total = 0 // 전체 시간을 저장하는 변수
             while (true) {
                 Thread.sleep(1000) //1초마다 실행
                 if (!started) break
                 total += 1
-                val msg = Message()
-                msg.what = SET_TIME // 위처럼 msg를 직접세팅하면 what에도 쓸 수 있고 arg1...등 세팅할 수 있음 -> 명령어가 여러개 있을 때 사용
-                msg.arg1 = total
-                handler.sendMessage(msg) // msg 오브젝트를 전달. sendEmptyMessage를 하면 what에만 값이 세팅되서 보내는거고 아예 메세지 객체를 직접 만들면 what에도 세팅할 수 있고 arg1에도 세팅할 수 있음
-                //handler.sendEmptyMessage(total) // 핸들러에 total 값을 전달
+                handler.sendEmptyMessage(total) // 핸들러에 total 값을 전달
             }
         }
     }
@@ -73,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 
     fun stop() {
         started = false
+        total = 0
         binding.textTimer.text = "00:00"
     }
 
